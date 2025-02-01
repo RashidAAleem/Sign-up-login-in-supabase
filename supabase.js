3a1ec51541b2aece5222fdeee8ed084c8ccc53f3
Rana Rashid <rrashid.physics@gmail.com>
2025-02-01 20:46:40 +0500

    update

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const projectUrl = "https://iljsehndhytyiougjhmg.supabase.co";
-const projectApi = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsanNlaG5kaHl0eWlvdWdqaG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0ODgxNjAsImV4cCI6MjA1MjA2NDE2MH0.yowQbGQfJIgCCdweM4YC4UzT3Wrmrpx-bIWQyDM8aZ4";
+const projectUrl = "https://xyz";
+const projectApi = "abc";
 export const supabaseConf = createClient(projectUrl, projectApi)
 
 
